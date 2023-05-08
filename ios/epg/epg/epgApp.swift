e46b8b2d8d030a0a6dffc7909617fc1f2c8adaa9
Tobias Schröpf <tobias.schroepf@jambit.com>
2023-05-08 09:02:53 +0200

    Initial empty XCode project

---
+++ ios/epg/epg/epgApp.swift
@@ -0,0 +1,17 @@
+//
+//  epgApp.swift
+//  epg
+//
+//  Created by Tobias Schröpf on 08.05.23.
+//
+
+import SwiftUI
+
+@main
+struct epgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
