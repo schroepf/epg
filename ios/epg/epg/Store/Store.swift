e8631772877c1f5fe49f2861b05654ed1186dcdf
Tobias Schröpf <tobias.schroepf@jambit.com>
2023-05-08 11:45:32 +0200

    Add middleware

--- ios/epg/epg/Store/Store.swift
+++ ios/epg/epg/Store/Store.swift
@@ -1,6 +1,9 @@
 import Foundation
 
+typealias Dispatcher = (Action) -> Void
+
 typealias Reducer<State: ReduxState> = (_ state: State, _ action: Action) -> State
+typealias Middleware<StoreState: ReduxState> = (StoreState, Action, @escaping Dispatcher) -> Void
 
 // MARK: - States
 
@@ -37,15 +40,29 @@ struct AddAction: Action {
 // MARK: - Store
 
 class Store<StoreState: ReduxState>: ObservableObject {
-    private var reducer: Reducer<StoreState>
+    private let reducer: Reducer<StoreState>
+    private let middlewares: [Middleware<StoreState>]
+
     @Published var state: StoreState
 
-    init(reducer: @escaping Reducer<StoreState>, state: StoreState) {
+    init(
+        reducer: @escaping Reducer<StoreState>,
+        state: StoreState,
+        middlewares: [Middleware<StoreState>] = []
+    ) {
         self.reducer = reducer
         self.state = state
+        self.middlewares = middlewares
     }
 
     func dispatch(action: Action) {
-        state = reducer(state, action)
+        DispatchQueue.main.async {
+            self.state = self.reducer(self.state, action)
+        }
+
+        // run all middlewares
+        middlewares.forEach { middleware in
+            middleware(state, action, dispatch)
+        }
     }
 }
--- ios/epg/epg/epgApp.swift
+++ ios/epg/epg/epgApp.swift
@@ -1,17 +1,10 @@
-//
-//  epgApp.swift
-//  epg
-//
-//  Created by Tobias Schröpf on 08.05.23.
-//
-
 import SwiftUI
 
 @main
 struct epgApp: App {
     var body: some Scene {
 
-        let store = Store(reducer: appReducer, state: AppState())
+        let store = Store(reducer: appReducer, state: AppState(), middlewares: [logMiddleware()])
 
         WindowGroup {
             ContentView().environmentObject(store)
