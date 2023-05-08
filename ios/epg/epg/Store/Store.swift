ec0097cf114d3d60a52b6cbaac6fc90c2620c5d0
Tobias Schröpf <tobias.schroepf@jambit.com>
2023-05-08 09:13:19 +0200

    Initial Store setup

---
+++ ios/epg/epg/Store/Store.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+struct State {
+
+}
+
+protocol Action {
+
+}
+
+func reduce(state: State, action: Action) -> State {
+    return state
+}
+
+typealias Reducer = (_ state: State, _ action: Action) -> State
+
+class Store {
+    private var reducer: Reducer
+    private var state: State
+
+
+    init(reducer: @escaping Reducer, state: State) {
+        self.reducer = reducer
+        self.state = state
+    }
+}
