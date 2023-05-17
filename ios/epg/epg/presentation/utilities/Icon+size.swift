01740ae32c293638638cbc13a889702308a69a33
Tobias Schröpf <tobias.schroepf@jambit.com>
2023-05-17 12:15:55 +0200

    Add new epg image provider

--- ios/epg/epg/presentation/utilities/Icon+size.swift
+++ ios/epg/epg/presentation/utilities/Icon+size.swift
@@ -4,6 +4,8 @@ extension Icon {
     func forSize(size: CGSize) -> URL? {
         if (url.host() == "image.fzdigital.de") {
             return URL(string: "https://image.fzdigital.de/\(Int(size.width))x\(Int(size.height))/\(url.lastPathComponent)")
+        } else if (url.host() == "epg-images.tvdirekt.de") {
+            return URL(string: "https://epg-images.tvdirekt.de/\(Int(size.width))x\(Int(size.height))/\(url.lastPathComponent)")
         }
 
         return url
